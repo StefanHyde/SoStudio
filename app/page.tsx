0e7e05a6446a7d288c5fc6a2069d1a017754dfe2
stephaneheyd <stephane@studioowl.fr>
2026-06-04 14:30:49 +0200

    chore - uninstall Shadnui (useless for project)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">So studio</div>
-  );
+  return <div className="flex flex-col flex-1 items-center justify-center font-sans bg-plage">So studio</div>;
 }
