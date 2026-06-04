f7e37a3f549a9e72f2803b12228e521cde1ebf89
stephaneheyd <stephane@studioowl.fr>
2026-06-04 15:34:04 +0200

    Header OK

--- components/header.tsx
+++ components/header.tsx
@@ -7,12 +7,12 @@ export default function Header() {
   return (
     <header
       className="
-                 fixed z-50 flex h-18 w-full items-center justify-between py-10 bg-white shadow-2xl"
+                 fixed z-50 flex h-18 w-full items-center justify-between px-4  md:px-24.5 py-1 bg-white shadow-md"
     >
       <div className="flex">
-        <Image src={logo} alt="Logo Studio Owl" className="object-contain" priority />
+        <Image src={logo} alt="Logo Studio Owl" height={38} className="object-contain h-8 md:h-full" priority />
       </div>
-      <Button variant="secondary">Prendre RDV</Button>
+      <Button variant="primary">Prendre RDV</Button>
     </header>
   );
 }
