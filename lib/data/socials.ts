e955b1b9ad1e299bb165ac50c16eebdc63a9f22b
stephaneheyd <stephane@studioowl.fr>
2026-06-07 16:56:54 +0200

    Add socials to footer

--- lib/data/socials.ts
+++ lib/data/socials.ts
@@ -1,14 +1,25 @@
-import { createElement, ReactElement } from "react";
+import { IconType } from "react-icons";
+import { IoLogoInstagram } from "react-icons/io5";
+import { SiSubstack } from "react-icons/si";
+import { FaSpotify } from "react-icons/fa6";
+import { FaPinterest, FaTwitch } from "react-icons/fa";
 
 interface socials {
   name: string;
   url: string;
-  icon?: ReactElement;
+  icon: IconType;
+  iconColor?: string;
 }
 
 export const socials: socials[] = [
   {
     name: "Instagram",
     url: "https://www.instagram.com/so.studio_siteweb",
+    icon: IoLogoInstagram,
+    iconColor: "text-corail",
   },
+  { name: "Spotify", url: "https://www.spotify.com", icon: FaSpotify, iconColor: "text-teal-900" },
+  { name: "Pinterest", url: "https://pin.it/PTWSuBbiP", icon: FaPinterest, iconColor: "text-corail" },
+  { name: "Twithc", url: "https://www.twitch.tv/soostudio", icon: FaTwitch, iconColor: "text-mer" },
+  { name: "Substack", url: "https://substack.com/@sostudio", icon: SiSubstack, iconColor: "text-orange" },
 ];
