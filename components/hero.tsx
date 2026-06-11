0d083b7065bafc4a948a70bd4ccfb441442822ad
stephaneheyd <stephane@studioowl.fr>
2026-06-11 12:28:11 +0200

    Update hero content

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,22 +4,19 @@ import palmierLogo from "@/images/logotype3.png";
 
 import Button from "./button";
 
-import Content from "@/content/hero.mdx";
-import { heroTitle, heroSubtitle, heroCTA, heroBtn } from "@/content/hero";
+import { hero } from "@/lib/data/hero";
 
 export default function Hero() {
   return (
     <div className="relative w-full md:w-2/3 xl:w-1/2 mx-auto flex flex-col justify-center items-center p-8 mt-40 mb-20">
       <Image src={palmierLogo} height={80} alt="Logo Studio Owl" className="absolute right-0 -top-8" />
       <div className=" text-palmier text-center gap-3">
-        <h1 className="font-dm-serif text-2xl md:text-h2 ">{heroTitle}</h1>
-        <h2 className="font-dm-serif text-5xl md:text-h1">{heroSubtitle}</h2>
-        <p className="text-normal md:text-p text-nuit my-6">
-          <Content />
-        </p>
+        <h1 className="font-dm-serif text-2xl md:text-h2 ">{hero.heroTitle}</h1>
+        <h2 className="font-dm-serif text-5xl md:text-h1">{hero.heroSubtitle}</h2>
+        <p className="text-normal md:text-p text-nuit my-6 whitespace-pre-line">{hero.heroDescription}</p>
         <div className="flex justify-center gap-4">
-          <Button variant="primary">{heroCTA}</Button>
-          <Button variant="secondary">{heroBtn}</Button>
+          <Button variant="primary">{hero.heroCTA}</Button>
+          <Button variant="secondary">{hero.heroBtn}</Button>
         </div>
         <Image src={corailLogo} height={80} alt="Logo Studio Owl" className="absolute left-0 -bottom-8" />
       </div>
---
+++ lib/data/hero.ts
@@ -0,0 +1,16 @@
+interface hero {
+  heroTitle: string;
+  heroSubtitle: string;
+  heroCTA: string;
+  heroBtn: string;
+  heroDescription: string;
+}
+
+export const hero: hero = {
+  heroTitle: "Et si c’était So facile",
+  heroSubtitle: "De faire un site rentable",
+  heroCTA: "Prendre RDV",
+  heroBtn: "Voir nos offres",
+  heroDescription:
+    "Welcome to Burger Bliss, where we take your cravings to a whole new level !\n\Our mouthwatering burgers are made from 100% beef and are served on freshly baked buns.",
+};
