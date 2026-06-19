f53b8a31ea5a21983adb3ece3a7f783168fae16b
stephaneheyd <stephane@studioowl.fr>
2026-06-19 15:29:39 +0200

    Some css adjustements

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ import AboutSection from "@/content/aboutSection.mdx";
 import TimelineSection from "@/content/timelineSection.mdx";
 import FaqSection from "@/content/faqSection.mdx";
 
+import type { Viewport } from "next";
+
+export const viewport: Viewport = { themeColor: "#fcf3e3" };
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans bg-plage">
