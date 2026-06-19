8f9d86c4d5851aaf41088716305a0eab846f677c
stephaneheyd <stephane@studioowl.fr>
2026-06-19 13:57:28 +0200

    Disable Spotify link

--- lib/data/socials.tsx
+++ lib/data/socials.tsx
@@ -1,7 +1,7 @@
 import { IconType } from "react-icons";
 import { IoLogoInstagram } from "react-icons/io5";
 import { SiSubstack } from "react-icons/si";
-import { FaSpotify } from "react-icons/fa6";
+//import { FaSpotify } from "react-icons/fa6";
 import { FaPinterest, FaTwitch } from "react-icons/fa";
 
 interface socials {
@@ -18,7 +18,7 @@ export const socials: socials[] = [
     icon: IoLogoInstagram,
     iconColor: "text-corail",
   },
-  { name: "Spotify", url: "https://www.spotify.com", icon: FaSpotify, iconColor: "text-teal-900" },
+  // { name: "Spotify", url: "https://www.spotify.com", icon: FaSpotify, iconColor: "text-teal-900" },
   { name: "Pinterest", url: "https://pin.it/PTWSuBbiP", icon: FaPinterest, iconColor: "text-corail" },
   { name: "Twithc", url: "https://www.twitch.tv/soostudio", icon: FaTwitch, iconColor: "text-mer" },
   { name: "Substack", url: "https://substack.com/@sostudio", icon: SiSubstack, iconColor: "text-orange" },
