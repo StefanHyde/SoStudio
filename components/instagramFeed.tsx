74eef176e3ab04172be9fb83774aaa8f489884cf
stephaneheyd <stephane@studioowl.fr>
2026-07-30 17:36:45 +0200

    Instagram grid OK

--- components/instagramFeed.tsx
+++ components/instagramFeed.tsx
@@ -1,11 +1,13 @@
 import { getInstagramPosts } from "@/lib/instagram";
 import Image from "next/image";
 
+import { IoLogoInstagram } from "react-icons/io5";
+
 export default async function InstagramFeed() {
   const posts = await getInstagramPosts();
 
   return (
-    <div className="max-w-4xl mx-auto py-12 px-4 w-1/2 grid grid-cols-3 gap-6">
+    <div className="w-full lg:w-3xl  py-12 px-4 grid grid-cols-3 gap-1 lg:gap-6 ">
       {posts.map((post) => {
         const imageUrl = post.display_url || post.thumbnail_url;
         if (!imageUrl) {
@@ -18,19 +20,17 @@ export default async function InstagramFeed() {
             href={post.permalink}
             target="_blank"
             rel="noopener noreferrer"
-            className="relative group block h-60 bg-sable overflow-hidden rounded-lg"
+            className="relative group block border-4 md:border-6 border-orange/90 h-48 md:h-60 overflow-hidden rounded-lg"
           >
             {imageUrl && (
               <Image
                 src={imageUrl}
-                alt={post.caption || "Post Instagram"}
+                alt={"Post Instagram"}
                 fill
                 className="object-cover transition-transform duration-300 group-hover:scale-105"
               />
             )}
-            <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex items-end p-4">
-              <p className="text-white text-sm line-clamp-3 font-medium">Voir sur Instagram →</p>
-            </div>
+            <IoLogoInstagram className="absolute bottom-1 left-1 h-6 w-6 duration-300 ease-in-out text-orange/80 opacity-0 group-hover:opacity-100" />
           </a>
         );
       })}
