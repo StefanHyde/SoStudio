fdf5021dbf4cdb8c16c9a27d02ad39f01a279a0f
stephaneheyd <stephane@studioowl.fr>
2026-08-26 16:38:11 +0200

    Add tertiary styles for button component

--- components/button.tsx
+++ components/button.tsx
@@ -1,16 +1,16 @@
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "tertiary";
 }
 
 export default function Button({ children, variant = "primary", className = "", ...props }: ButtonProps) {
-  const btnStyles =
-    variant === "primary"
-      ? "bg-orange text-plage hover:bg-corail uppercase"
-      : "bg-transparent border-1 border-orange text-orange hover:bg-orange hover:text-plage normal-case";
-
+  const btnStyles = {
+    primary: "bg-orange text-plage hover:bg-corail uppercase",
+    secondary: "bg-transparent border border-orange text-orange hover:bg-orange hover:text-plage normal-case",
+    tertiary: "bg-orange text-plage hover:bg-corail normal-case p-2! text-sm! rounded-xl!",
+  };
   return (
     <button
-      className={`p-3 md:p-4 rounded-2xl text-sm md:text-p  transition ease-in-out 300ms cursor-pointer ${btnStyles} ${className}`}
+      className={`p-3 md:p-4 rounded-2xl text-sm md:text-p  transition ease-in-out 300ms cursor-pointer ${btnStyles[variant]}${className}`}
       {...props}
     >
       {children}
